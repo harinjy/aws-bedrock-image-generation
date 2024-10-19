7d10cd9efd75da437301b6658571f0d002618189
Harin Jalaja Yesodharan <harin@Harins-MacBook-Pro.local>
2024-10-19 13:10:32 -0500

    Initial commit

---
+++ test/aws-bedrock-image-generation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsBedrockImageGeneration from '../lib/aws-bedrock-image-generation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-bedrock-image-generation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsBedrockImageGeneration.AwsBedrockImageGenerationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
